47d72a9cc96b0ebc64077e0ef28b3522544c80a4
DiegoCerdaC <diego@sphereone.xyz>
2023-06-20 10:07:06 -0400

    Fix endpoints + logout redirect + cleanup private names

--- index.ts
+++ index.ts
@@ -9,12 +9,11 @@ class WebSDK implements iWebSDK {
   apiKey?: string;
   user?: User;
   credentials?: Credentials | null;
-  #_auth0Client?: any;
-  #_PROJECT_ID: string = '<FIREBASE_PROJECT_ID>';
-  #_domain: string = '<DOMAIN_AUTH0>';
-  #_audience: string = '<AUDIENCE_AUTH0_API>';
-  #_wrappedDek: string = '';
-  #_baseUrl: string = `https://us-central1-${this.#_PROJECT_ID}.cloudfunctions.net/api`;
+  #auth0Client?: any;
+  #wrappedDek: string = '';
+  #domain: string = '<DOMAIN_AUTH0>';
+  #audience: string = '<AUDIENCE_AUTH0_API>';
+  #baseUrl: string = '<API_BASE_URL>';
 
   constructor({ clientId, redirectUri, apiKey }: iWebSDK) {
     if (WebSDK.instance) return WebSDK.instance;
@@ -26,21 +25,21 @@ class WebSDK implements iWebSDK {
     this.user = {};
     this.credentials = null;
 
-    this.#_auth0Client = new auth0.WebAuth({
-      domain: this.#_domain as string,
+    this.#auth0Client = new auth0.WebAuth({
+      domain: this.#domain as string,
       clientID: this.clientId as string,
       redirectUri: this.redirectUri as string,
-      audience: this.#_audience as string,
+      audience: this.#audience as string,
       responseType: 'token id_token',
     });
   }
 
   handleCallback = () => {
     // This checks if there is a previous Auth0 session initialized
-    this.#_auth0Client.checkSession({}, (err: any, authResult: any) => {
+    this.#auth0Client.checkSession({}, (err: any, authResult: any) => {
       if (err) {
         // Here falls if there is NO previous session stored
-        this.#_auth0Client.parseHash((err: any, authResult: any) => {
+        this.#auth0Client.parseHash((err: any, authResult: any) => {
           if (err) {
             // Error handling
             console.error('Error login:', err);
@@ -65,16 +64,17 @@ class WebSDK implements iWebSDK {
   };
 
   login() {
-    this.#_auth0Client.authorize();
+    this.#auth0Client.authorize();
   }
 
   logout() {
-    this.#_auth0Client.logout({
+    this.#auth0Client.logout({
       redirectUri: this.redirectUri,
+      returnTo: this.redirectUri,
     });
   }
 
-  #_createRequest = async (body: any = {}, method: string = 'POST', headers: any = {}) => {
+  #createRequest = async (method: string = 'GET', body: any = {}, headers: any = {}) => {
     const myHeaders = new Headers();
     myHeaders.append('Content-Type', 'application/json');
     myHeaders.append('Authorization', `Bearer ${this.credentials?.accessToken}`);
@@ -107,12 +107,12 @@ class WebSDK implements iWebSDK {
     return requestOptions;
   };
 
-  #_fetchUserBalances = async () => {
+  #fetchUserBalances = async () => {
     try {
-      const requestOptions = await this.#_createRequest({ refreshCache: true });
+      const requestOptions = await this.#createRequest();
 
       const response = await fetch(
-        `${this.#_baseUrl}/getFundsAvailable`,
+        `${this.#baseUrl}/getFundsAvailable?refreshCache=true`,
         requestOptions
       );
 
@@ -124,29 +124,23 @@ class WebSDK implements iWebSDK {
     }
   };
 
-  #_fetchUserWallets = async () => {
+  #fetchUserWallets = async () => {
     try {
-      const requestOptions = await this.#_createRequest({}, 'GET');
-      const response = await fetch(
-        `${this.#_baseUrl}/user/wallets`,
-        requestOptions
-      );
+      const requestOptions = await this.#createRequest();
+      const response = await fetch(`${this.#baseUrl}/user/wallets`, requestOptions);
 
       const data = await response.json();
-      if (this.user)  this.user.wallets = data.data;
+      if (this.user) this.user.wallets = data.data;
       return data.data;
     } catch (error: any) {
       console.log(error);
     }
   };
 
-  #_fetchUserInfo = async () => {
+  #fetchUserInfo = async () => {
     try {
-      const requestOptions = await this.#_createRequest({}, 'GET');
-      const response = await fetch(
-        `${this.#_baseUrl}/user`,
-        requestOptions
-      );
+      const requestOptions = await this.#createRequest();
+      const response = await fetch(`${this.#baseUrl}/user`, requestOptions);
 
       const data = await response.json();
       if (this.user) this.user.info = data.data;
@@ -156,13 +150,10 @@ class WebSDK implements iWebSDK {
     }
   };
 
-  #_fetchUserNfts = async () => {
+  #fetchUserNfts = async () => {
     try {
-      const requestOptions = await this.#_createRequest({}, 'GET');
-      const response = await fetch(
-        `${this.#_baseUrl}/getNftsAvailable`,
-        requestOptions
-      );
+      const requestOptions = await this.#createRequest();
+      const response = await fetch(`${this.#baseUrl}/getNftsAvailable`, requestOptions);
 
       const data = await response.json();
       if (this.user) this.user.nfts = data.data;
@@ -172,18 +163,15 @@ class WebSDK implements iWebSDK {
     }
   };
 
-  #_getWrappedDek = async () => {
-    if (this.#_wrappedDek) return this.#_wrappedDek;
+  #getWrappedDek = async () => {
+    if (this.#wrappedDek) return this.#wrappedDek;
     try {
-      const requestOptions = await this.#_createRequest();
+      const requestOptions = await this.#createRequest();
 
-      const response = await fetch(
-        `${this.#_baseUrl}/createOrRecoverAccount`,
-        requestOptions
-      );
+      const response = await fetch(`${this.#baseUrl}/createOrRecoverAccount`, requestOptions);
 
       const data = await response.json();
-      this.#_wrappedDek = data.data;
+      this.#wrappedDek = data.data;
       return data.data;
     } catch (error: any) {
       console.log(error);
@@ -192,9 +180,9 @@ class WebSDK implements iWebSDK {
 
   pay = async ({ toAddress, chain, symbol, amount, tokenAddress }: Transaction) => {
     try {
-      const wrappedDek = await this.#_getWrappedDek();
+      const wrappedDek = await this.#getWrappedDek();
 
-      const requestOptions = await this.#_createRequest({
+      const requestOptions = await this.#createRequest('POST', {
         wrappedDek,
         toAddress,
         chain,
@@ -203,10 +191,7 @@ class WebSDK implements iWebSDK {
         tokenAddress,
       });
 
-      const response = await fetch(
-        `${this.#_baseUrl}/pay`,
-        requestOptions
-      );
+      const response = await fetch(`${this.#baseUrl}/pay`, requestOptions);
       const data = await response.json();
       return data.data;
     } catch (error: any) {
@@ -216,14 +201,11 @@ class WebSDK implements iWebSDK {
 
   payCharge = async (transactionId: string) => {
     try {
-      const wrappedDek = await this.#_getWrappedDek();
+      const wrappedDek = await this.#getWrappedDek();
 
-      const requestOptions = await this.#_createRequest({ wrappedDek, transactionId });
+      const requestOptions = await this.#createRequest('POST', { wrappedDek, transactionId });
 
-      const response = await fetch(
-        `${this.#_baseUrl}/pay`,
-        requestOptions
-      );
+      const response = await fetch(`${this.#baseUrl}/pay`, requestOptions);
       const data = await response.json();
       return data.result.data;
     } catch (error: any) {
@@ -233,25 +215,25 @@ class WebSDK implements iWebSDK {
 
   getWallets = async () => {
     if (this.user?.wallets) return this.user.wallets;
-    const wallets = await this.#_fetchUserWallets();
+    const wallets = await this.#fetchUserWallets();
     return wallets;
   };
 
   getUserInfo = async () => {
     if (this.user?.info) return this.user.info;
-    const userInfo = await this.#_fetchUserInfo();
+    const userInfo = await this.#fetchUserInfo();
     return userInfo;
   };
 
   getBalances = async () => {
     if (this.user?.balances) return this.user.balances;
-    const balances = await this.#_fetchUserBalances();
+    const balances = await this.#fetchUserBalances();
     return balances;
   };
 
   getNfts = async () => {
     if (this.user?.nfts) return this.user.nfts;
-    const nfts = await this.#_fetchUserNfts();
+    const nfts = await this.#fetchUserNfts();
     return nfts;
   };
 }
