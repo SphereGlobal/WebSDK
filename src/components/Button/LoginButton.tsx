c2d11633b15d4306806fa3c0fdd9032682027851
Santiago <santiago@sphereone.xyz>
2023-08-15 10:25:38 -0300

    resolving pr comments

--- src/components/Button/LoginButton.tsx
+++ src/components/Button/LoginButton.tsx
@@ -5,13 +5,13 @@ const WhiteLogo = require('../../assets/SphereOneLogo.svg') as string;
 interface LoginButtonProps {
   onClick: () => void;
   buttonStyle?: React.CSSProperties;
-  logoColor?: string;
+  isLogoDark?: boolean;
 }
 
-const LoginButton: React.FC<LoginButtonProps> = ({ onClick, buttonStyle, logoColor = 'black' }) => {
+const LoginButton: React.FC<LoginButtonProps> = ({ onClick, buttonStyle, isLogoDark = false }) => {
   return (
     <button style={buttonStyle} onClick={onClick}>
-      <img src={!logoColor ? BlackLogo : WhiteLogo} alt="logo" />
+      <img src={isLogoDark ? BlackLogo : WhiteLogo} alt="logo" />
     </button>
   );
 };
