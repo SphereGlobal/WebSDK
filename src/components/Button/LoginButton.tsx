99ab8313b390ceab14fd70dedfb56de0fad10519
Matias Zapata <117381223+matzapatasphereone@users.noreply.github.com>
2023-08-15 21:53:58 -0300

    minimal styles

--- src/components/Button/LoginButton.tsx
+++ src/components/Button/LoginButton.tsx
@@ -6,13 +6,38 @@ interface LoginButtonProps {
   isLogoDark?: boolean;
 }
 
-const blackLogoSrc = "https://firebasestorage.googleapis.com/v0/b/sphereone-testing.appspot.com/o/BlackLogo.png?alt=media&token=60ba8159-273a-4d01-9473-5a82685d9054"
-const whiteLogoSrc = "https://firebasestorage.googleapis.com/v0/b/sphereone-testing.appspot.com/o/WhiteLogo.png?alt=media&token=e796c9fd-db1b-456a-818d-db1bab51c976"
+const blackLogoSrc =
+  'https://firebasestorage.googleapis.com/v0/b/sphereone-testing.appspot.com/o/BlackLogo.png?alt=media&token=60ba8159-273a-4d01-9473-5a82685d9054';
+const whiteLogoSrc =
+  'https://firebasestorage.googleapis.com/v0/b/sphereone-testing.appspot.com/o/WhiteLogo.png?alt=media&token=e796c9fd-db1b-456a-818d-db1bab51c976';
 
-const LoginButton: React.FC<LoginButtonProps> = ({ onClick, buttonStyle, isLogoDark, ...props }) => {
+const LoginButton: React.FC<LoginButtonProps> = ({
+  buttonStyle,
+  imageStyles,
+  ...props
+}: {
+  buttonStyle?: React.CSSProperties;
+  imageStyles?: React.CSSProperties;
+}) => {
   return (
-    <button style={buttonStyle} onClick={onClick} {...props}>
-      <img src={isLogoDark ? blackLogoSrc : whiteLogoSrc} alt="SphereOne logo" />
+    <button
+      style={{
+        backgroundColor: '#7ACBFF',
+        paddingTop: '0.625rem',
+        paddingBottom: '0.625rem',
+        borderRadius: '0.5rem',
+        alignItems: 'center',
+        display: 'inline-flex',
+        paddingLeft: '1.25rem',
+        paddingRight: '1.25rem',
+        ...buttonStyle,
+      }}
+      {...props}>
+      <img
+        src={blackLogoSrc}
+        alt="SphereOne logo"
+        style={{ ...imageStyles }}
+      />
     </button>
   );
 };
