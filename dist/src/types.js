3c92b9526815dd752700bc2ae4dbb5e9009f0887
Juan Bevilacqua <118808875+JuanSphereOne@users.noreply.github.com>
2023-09-19 18:13:52 -0300

    ENG 1648  - Refactor refresh logic, refresh data and types (#59)
    * test1
    
    * adding refresh logic to webSDK
    
    * adding refresh logic to webSDK1
    
    * fixing signinSilent?
    
    * add offline_scope
    
    * 2
    
    * test
    
    * v1
    
    * merge develop and fixes
    
    * adding types
    
    * add BigNumber
    
    * npm run build
    
    * fix setBaseUrl
    
    * adding types
    
    * delete clientSecret
    
    * npm run build
    
    * erase console.log
    
    * modified the parameter in getxxxx to be objects in all functions
    
    * fix return types and refactor check of expired token
    
    * remove GNOSIS from SupportedChains
    
    * remove console.log
    
    * remove user type undefined
    
    * change interface for abstract class
    
    * remove invitedBy
    
    * deleted abstract class and interface
    
    * deleted instance property

--- dist/src/types.js
+++ dist/src/types.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Environments = exports.WalletTypes = exports.SupportedChains = exports.LoginBehavior = void 0;
+exports.TransferType = exports.BridgeServices = exports.RouteActionType = exports.TxStatus = exports.Environments = exports.WalletTypes = exports.SupportedChains = exports.LoginBehavior = void 0;
 var LoginBehavior;
 (function (LoginBehavior) {
     LoginBehavior["REDIRECT"] = "REDIRECT";
@@ -11,13 +11,15 @@ var SupportedChains;
     SupportedChains["ETHEREUM"] = "ETHEREUM";
     SupportedChains["SOLANA"] = "SOLANA";
     SupportedChains["POLYGON"] = "POLYGON";
-    SupportedChains["GNOSIS"] = "GNOSIS";
     SupportedChains["OPTIMISM"] = "OPTIMISM";
     SupportedChains["IMMUTABLE"] = "IMMUTABLE";
     SupportedChains["AVALANCHE"] = "AVALANCHE";
     SupportedChains["BINANCE"] = "BINANCE";
     SupportedChains["ARBITRUM"] = "ARBITRUM";
     SupportedChains["FANTOM"] = "FANTOM";
+    SupportedChains["EOSEVM"] = "EOSEVM";
+    SupportedChains["BASE"] = "BASE";
+    SupportedChains["FLOW"] = "FLOW";
 })(SupportedChains || (exports.SupportedChains = SupportedChains = {}));
 var WalletTypes;
 (function (WalletTypes) {
@@ -30,3 +32,33 @@ var Environments;
     Environments["STAGING"] = "staging";
     Environments["PRODUCTION"] = "production";
 })(Environments || (exports.Environments = Environments = {}));
+var TxStatus;
+(function (TxStatus) {
+    TxStatus["PENDING"] = "PENDING";
+    TxStatus["PROCESSING"] = "PROCESSING";
+    TxStatus["SUCCESS"] = "SUCCESS";
+    TxStatus["FAILURE"] = "FAILURE";
+    TxStatus["CANCELED"] = "CANCELED";
+    TxStatus["WAITING"] = "WAITING";
+})(TxStatus || (exports.TxStatus = TxStatus = {}));
+var RouteActionType;
+(function (RouteActionType) {
+    RouteActionType["BRIDGE"] = "BRIDGE";
+    RouteActionType["SWAP"] = "SWAP";
+    RouteActionType["TRANSFER"] = "TRANSFER";
+})(RouteActionType || (exports.RouteActionType = RouteActionType = {}));
+var BridgeServices;
+(function (BridgeServices) {
+    BridgeServices["WOMRHOLE"] = "wormhole";
+    BridgeServices["LIFI"] = "lifi";
+    BridgeServices["IMX"] = "imx";
+    BridgeServices["STEALTHEX"] = "stealthex";
+    BridgeServices["SQUID"] = "squid";
+    BridgeServices["SWFT"] = "swft";
+})(BridgeServices || (exports.BridgeServices = BridgeServices = {}));
+var TransferType;
+(function (TransferType) {
+    TransferType["SYSTEM"] = "SYSTEM";
+    TransferType["ERC20"] = "ERC20";
+    TransferType["SPL"] = "SPL";
+})(TransferType || (exports.TransferType = TransferType = {}));
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,6 @@
+import { BigNumber } from '@ethersproject/bignumber';
+import { UserManager, WebStorageStateStore } from 'oidc-client-ts';
+
 export interface WalletBalance {
   price: number; // usdc amount bigint
   amount: number; // amount bigint
@@ -21,13 +24,15 @@ export enum SupportedChains {
   ETHEREUM = 'ETHEREUM',
   SOLANA = 'SOLANA',
   POLYGON = 'POLYGON',
-  GNOSIS = 'GNOSIS',
   OPTIMISM = 'OPTIMISM',
   IMMUTABLE = 'IMMUTABLE',
   AVALANCHE = 'AVALANCHE',
   BINANCE = 'BINANCE',
   ARBITRUM = 'ARBITRUM',
   FANTOM = 'FANTOM',
+  EOSEVM = 'EOSEVM',
+  BASE = 'BASE',
+  FLOW = 'FLOW',
 }
 
 export interface Token {
@@ -39,7 +44,7 @@ export interface Token {
   chain: SupportedChains;
 }
 
-export interface WalletDoc {
+export interface Wallet {
   address: string;
   publicKey: string;
   privateKey: string;
@@ -48,50 +53,55 @@ export interface WalletDoc {
   type: WalletTypes;
   starkPrivateKey?: string;
 }
-
+export interface WalletResponse {
+  data: Wallet[] | null;
+  error: string | null;
+}
 export enum WalletTypes {
   EOA = 'EOA',
   SMART_WALLET = 'SmartWallet',
 }
 
-export interface iWebSDK {
-  loginType: 'REDIRECT' | 'POPUP';
-  providerUid?: string;
-  clientId?: string;
-  clientSecret?: string;
-  redirectUri?: string;
-  baseUrl?: string;
-  apiKey?: string;
-  user?: User | null;
-  credentials?: Credentials | null;
-  balances?: WalletBalance[] | null;
-  auth0Client?: any;
-  PROJECT_ID?: string;
-}
-
 export interface User {
-  info?: Info;
-  wallets?: WalletDoc[];
-  balances?: WalletBalance[];
+  info?: UserInfo;
+  wallets?: Wallet[];
+  balances?: UserBalance;
   nfts?: NftsInfo[];
   uid?: string;
+  transactions?: string; // this data comes as a JWT
 }
 
-export interface Info {
+export interface UserInfo {
+  uid: string;
+  signedUp: boolean;
+  waitlistPoints: number;
   email?: string;
+  currency: string;
+  countryFlag: string;
+  referralLink: string;
+  countryCode: string;
   name?: string;
   username?: string;
-  countryCode?: string;
-  blockFundsUntil: {
-    _seconds: number;
-    _nanoseconds: number;
-  };
+  isMerchant: boolean;
+  profilePicture: string;
+}
+export interface UserInfoResponse {
+  data: UserInfo | null;
+  error: string | null;
 }
 
 export interface Credentials {
   accessToken: string;
   idToken: string;
   refreshToken?: string;
+  expires_at: number;
+}
+
+export interface LoadCredentialsParams {
+  access_token: string;
+  idToken?: string;
+  refresh_token?: string;
+  expires_at?: number;
 }
 
 export interface Transaction {
@@ -101,7 +111,7 @@ export interface Transaction {
   symbol: string;
   amount: number;
   tokenAddress: string;
-  nft?: NftsInfo
+  nft?: NftsInfo;
 }
 
 export type NftsInfo = {
@@ -110,6 +120,11 @@ export type NftsInfo = {
   address: string | undefined;
   tokenType?: string;
 };
+
+export interface NftsInfoResponse {
+  data: NftsInfo[] | null;
+  error: string | null;
+}
 
 export enum Environments {
   DEVELOPMENT = 'development',
@@ -138,10 +153,275 @@ export type ChargeReqBody = {
   toAddress?: string;
 };
 
+export interface ChargeUrlAndId {
+  paymentUrl: string;
+  chargeId: string;
+}
 export interface ChargeResponse {
-  data: {
-    paymentUrl: string;
-    chargeId: string;
-  } | null;
+  data: ChargeUrlAndId | null;
   error: string | null;
+}
+
+export interface CreateRequest {
+  method: 'GET' | 'POST';
+  headers: Headers;
+  body?: string;
+}
+
+export enum TxStatus {
+  PENDING = 'PENDING', // Not executed already
+  PROCESSING = 'PROCESSING', // Waiting to get mined
+  SUCCESS = 'SUCCESS',
+  FAILURE = 'FAILURE',
+  CANCELED = 'CANCELED',
+  WAITING = 'WAITING', // Waiting for user to finish the provider flow
+}
+
+export interface Estimate {
+  time: number; // minutes
+  costUsd: number;
+  ethGas: BigNumber;
+  maticGas: BigNumber;
+  optGas: BigNumber;
+  avaxGas: BigNumber;
+  arbGas: BigNumber;
+  bscGas: BigNumber;
+  solGas: BigNumber;
+  eosEvmGas: BigNumber;
+  baseGas: BigNumber;
+}
+export enum RouteActionType {
+  BRIDGE = 'BRIDGE',
+  SWAP = 'SWAP',
+  TRANSFER = 'TRANSFER',
+}
+export interface SwapData {
+  fromChain: SupportedChains;
+  fromAmount: BigNumber;
+  fromToken: Token;
+  fromAddress: string;
+  fromPrivateKey: string;
+  toAmount: BigNumber;
+  toToken: Token;
+  estimatedGas: BigNumber;
+}
+
+export interface SwapResponse {
+  data: SwapResponseData | null;
+  error: string | null;
+}
+
+export interface SwapResponseData {
+  fromChain: SupportedChains;
+  fromPrivateKey: string;
+  fromAddress: string;
+  fromToken: Token;
+  toToken: Token;
+  toAmount: BigNumber;
+  fromAmount: BigNumber;
+  userOperation?: {
+    userOpHash: string;
+    wait: () => Promise<any>;
+  };
+  userOperationHash: string | null; // Only valid for smart wallets (is used for retrieving the Tx Hash after bundlers execution)
+  approveTxHash: string | null; // always null for solana
+  swapTxHash: string | null;
+  approveStatus: TxStatus;
+  swapStatus: TxStatus;
+  status: TxStatus; // success when approve and status are done
+  sponsoredFee?: boolean;
+}
+
+export interface TransferData {
+  fromChain: SupportedChains;
+  fromAmount: BigNumber;
+  fromAddress: string;
+  fromPrivateKey: string;
+  fromToken: Token;
+  toAddress: string;
+  smartWallet?: boolean; // True by default. Only useful for polygon rn
+  starkPrivateKey?: string;
+}
+
+export interface RouteAction {
+  type: RouteActionType;
+  status: TxStatus;
+  estimate: Estimate;
+  swapData?: SwapData;
+  swapResponse?: SwapResponse;
+  transferData?: TransferData;
+  transferResponse?: TransferResponse;
+  bridgeData?: BridgeProps;
+  bridgeResponse?: BridgeResponse;
+}
+
+export enum BridgeServices {
+  WOMRHOLE = 'wormhole',
+  LIFI = 'lifi',
+  IMX = 'imx',
+  STEALTHEX = 'stealthex',
+  SQUID = 'squid',
+  SWFT = 'swft',
+}
+export interface BridgeQuote {
+  rawQuote: any; // Raw lifi quote or null
+  service: BridgeServices;
+  fromChain: SupportedChains;
+  fromAmount: BigNumber;
+  fromAddress: string;
+  fromToken: Token;
+  toChain: SupportedChains;
+  toAmount: BigNumber;
+  toAddress: string;
+  toToken: Token;
+  estimatedTime: number;
+  estimatedCostUSD: number;
+  estimatedEthGas: BigNumber;
+  estimatedMatGas: BigNumber;
+  estimatedAvaxGas: BigNumber;
+  estimatedArbGas: BigNumber;
+  estimatedBscGas: BigNumber;
+  estimatedSolGas: BigNumber;
+  estimatedOptGas: BigNumber;
+  estimatedEosEvmGas: BigNumber;
+  estimatedBaseGas: BigNumber;
+
+  //  is only needed for StealthEX / SWFT bridge
+  bridgeId?: string;
+  depositAddress?: string;
+}
+
+export interface BridgeProps {
+  quote: BridgeQuote;
+  wallets: ChainWallets;
+  userSponsoredGas?: number | undefined;
+}
+
+export interface BridgeResponse {
+  data: BridgeResponseData | null;
+  error: string | null;
+}
+
+export type ChainWallets = {
+  [chain in SupportedChains]: Wallet;
+};
+
+export interface BridgeResponseData {
+  quote: BridgeQuote;
+  wallets: ChainWallets;
+  bridgeTx: string | null; // for wormhole this refers to send to bridge tx
+  redeemTx: string | null; // wormhole only
+  approveTx: string | null; // lifi only. Sometimes allowance is already there so no need for this
+  postVaaTx: string | null;
+  userOperation?: {
+    userOpHash: string;
+    wait: () => Promise<any>;
+  };
+  userOperationHash: string | null; // Only with Smart Wallets to poll TxHash later
+  bridgeStatus: TxStatus;
+  redeemStatus: TxStatus; // (wormhole only) when using lifi true by default
+  approveStatus: TxStatus; // (lifi only) when using wormhole true by default
+  status: TxStatus;
+  sequence?: string; // wormhole only
+  emitterAddress?: string; // wormhole only
+  bridgeId?: string; // stealthex/swft only
+  sponsoredFee?: boolean; // We can sponsor or not with Smart Wallets
+}
+
+export interface TransferResponse {
+  data: TransferResponseData | null;
+  error: string | null;
+}
+export interface TransferResponseData {
+  fromChain: SupportedChains;
+  fromAmount: BigNumber;
+  fromAddress: string;
+  fromTokenAddress: string;
+  toAddress: string;
+  hash: string | null;
+  userOperationHash: null | string;
+  fromPrivateKey: string;
+  transferType: TransferType;
+  status: TxStatus;
+  fee?: BigNumber;
+  rawRecipient?: any;
+  sponsoredFee: boolean;
+  userOperation?: {
+    userOpHash: string;
+    wait: () => Promise<any>;
+  };
+}
+
+export enum TransferType {
+  SYSTEM = 'SYSTEM',
+  ERC20 = 'ERC20',
+  SPL = 'SPL',
+}
+export interface RouteBatch {
+  description: string;
+  status: TxStatus;
+  actions: RouteAction[];
+  afterBalances: WalletBalance[];
+  estimate: Estimate;
+}
+
+export interface Route {
+  id?: string;
+  toChain?: SupportedChains;
+  toAmount?: string;
+  toAddress?: string;
+  toToken?: Token;
+  estimate?: Estimate;
+  fromBalances: WalletBalance[];
+  status: TxStatus;
+  batches: RouteBatch[];
+  fromUid?: string;
+}
+
+export interface OnRampResponse {
+  status: TxStatus;
+  onrampLink: string;
+}
+
+export interface RouteResponse {
+  status: TxStatus;
+  route: Route;
+}
+export interface PayErrorResponse {
+  error: string | { code: string; message: string };
+  data: null;
+}
+
+export interface PayResponseOnRampLink {
+  error: { code: string; message: string };
+  data: OnRampResponse;
+}
+
+export interface PayResponseRouteCreated {
+  error: null;
+  data: RouteResponse;
+}
+
+export interface UserBalance {
+  balances: WalletBalance[];
+  total: string;
+}
+
+export interface UserBalancesResponse {
+  data: UserBalance | null;
+  error: string | null;
+}
+
+export interface WrappedDekResponse {
+  data: string | null;
+  error: null | string;
+}
+
+export interface TransactionsResponse {
+  data: string | null; // this data comes as a JWT
+  error: string | null;
+}
+
+export interface ForceRefresh {
+  forceRefresh?: boolean;
 }
